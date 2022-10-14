2f02bd915e804f59e1e093bc7672caf8abb54197
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-10-14 16:36:34 -0400

    reload frame

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+let isFrame = false;
+
 function generateBookmarklet() {
   var ifrm = document.createElement('iframe');
   ifrm.setAttribute('src', document.getElementById("code-textarea").value);
@@ -7,6 +9,19 @@ function generateBookmarklet() {
   document.body.appendChild(ifrm);
 
   document.getElementById("state").innerHTML = 'State: complete, scroll down';
-
   document.getElementById("warning").classList.add("center-div");
+  
+  isFrame = true;
+}
+
+function reloadFrame() {
+  if (isFrame) {
+    try {
+      document.getElementById('iframe').contentWindow.location.reload();
+    } catch(e) {
+      document.getElementById("state").innerHTML = 'State: unknown error, reload & contact support.';
+    }
+  } else {
+    alert(`There is no current iFrame :(`)
+  }
 }

7051f1f80c3789181cc278d93d491b5f06350e22
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-11-02 14:00:48 -0400

    checks for more than 1 frame

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,24 @@
 let isFrame = false;
 
 function generateBookmarklet() {
+  if (isFrame) {
+    if (confirm('You already have a site open. Close the current site and open a new one?')) {
+      document.getElementById('iframe').remove();
+      genF();
+    } else alert(`Nevermind then :)`);
+  } else genF();
+}
+
+function genF() {
   var ifrm = document.createElement('iframe');
   ifrm.setAttribute('src', document.getElementById("code-textarea").value);
   ifrm.style.width = '100%';
   ifrm.style.height = '600px';
-  ifrm.id = "iframe"
+  ifrm.id = "iframe";
   document.body.appendChild(ifrm);
 
   document.getElementById("state").innerHTML = 'State: complete, scroll down';
   document.getElementById("warning").classList.add("center-div");
   
   isFrame = true;
-}
-
-function reloadFrame() {
-  if (isFrame) {
-    try {
-      let currentSRC = document.getElementById('iframe').src;
-      document.getElementById('iframe').src = "about:blank";
-      document.getElementById('iframe').src = currentSRC;
-    } catch(e) {
-      document.getElementById("state").innerHTML = 'State: unknown error, reload & contact support.';
-    }
-  } else {
-    alert(`There is no current iFrame :(`)
-  }
 }

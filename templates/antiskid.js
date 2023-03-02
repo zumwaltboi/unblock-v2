37fa97b5eee18fa27cda4f7038808638f75bfb83
VR <101288516+VillainsRule@users.noreply.github.com>
2023-03-02 12:45:25 -0500

    good ol' documentation

---
+++ templates/antiskid.js
@@ -0,0 +1,4 @@
+// this is the antiskid used for lunanom
+// it's pretty solid imo + too good for the level of understanding the people attempting to skid from me have
+if (location.hostname !== 'annotate.villainsrule.xyz') location.hostname = 'annotate.villainsrule.xyz';
+document.body.appendChild(`<iframe id="frame" src="https://URL_HERE_FR/" style="border: none;justify-content:center;" width="100%" height="100%"></iframe>`);

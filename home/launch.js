92dfe56201bcd8647f84adace02171115dc8a5ae
VR <101288516+VillainsRule@users.noreply.github.com>
2023-03-11 18:08:44 -0500

    wrap in onload & make dynamic

--- home/launch.js
+++ home/launch.js
@@ -1,23 +1,25 @@
-let currentWindow = null;
+window.onload = () => {
+  let currentWindow = null;
 
-const openSecureWindow = () => {
-  if (firstExternalWindow) {
-    firstExternalWindow.window.close();
-    firstExternalWindow = null;
+  const openSecureWindow = () => {
+    if (firstExternalWindow) {
+      firstExternalWindow.window.close();
+      firstExternalWindow = null;
+    };
+
+    if (currentWindow) {
+      currentWindow.window.close();
+      currentWindow = null;
+    };
+
+    currentWindow = window.open('about:blank');
+    currentWindow.document.write(`<head><title>404</title></head><body><style>body {margin: 0;}</style><iframe style="border: none;" src="https://${location.hostname}/home" width="100%" height="100%"></body>`);
   };
 
-  if (currentWindow) {
-    currentWindow.window.close();
-    currentWindow = null;
+  document.getElementsByTagName('body')[0].onbeforeunload = function(e) {
+    e.preventDefault();
+    try { firstExternalWindow.window.close(); } catch {};
+    try { currentWindow.window.close(); } catch {};
+    return 'Closed all external windows.';
   };
-
-  currentWindow = window.open('about:blank');
-  currentWindow.document.write(`<head><title>404</title></head><body><style>body {margin: 0;}</style><iframe style="border: none;" src="https://anno.villainsrule.xyz/home" width="100%" height="100%"></body>`);
-};
-
-document.getElementsByTagName('body')[0].onbeforeunload = function(e) {
-  e.preventDefault();
-  try { firstExternalWindow.window.close(); } catch {};
-  try { currentWindow.window.close(); } catch {};
-  return 'Closed all external windows.';
 };
